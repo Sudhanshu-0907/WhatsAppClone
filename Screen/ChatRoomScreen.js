6bf67df243e0faef7895a1e9cad1c3bb3220063c
Sudhanshu-0907 <sudhanshusingh0907@gmail.com>
2024-02-29 00:24:39 +0530

    displayed messages in ChatRoomScreen

--- Screen/ChatRoomScreen.js
+++ Screen/ChatRoomScreen.js
@@ -1,12 +1,24 @@
 import React from "react";
-import {Text} from "react-native";
 import {useRoute} from "@react-navigation/native";
+import ChatRoomData from '../data/Chats'
+import {FlatList, ImageBackground} from "react-native";
+import ChatMessage from "../Components/ChatMessage";
+import BG from "../assets/images/BG.png";
 
 
 const ChatRoomScreen = () => {
     const route = useRoute();
     return (
-        <Text>{route.params.name}</Text>
+        <ImageBackground
+            style={{flex: 1}}
+            source={BG}
+        >
+            <FlatList
+                data={ChatRoomData.messages}
+                renderItem={({item}) => <ChatMessage message={item}/>}
+                inverted
+            />
+        </ImageBackground>
     );
 }
 export default ChatRoomScreen;
