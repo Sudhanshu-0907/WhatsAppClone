6bf189ad8b89e229aedbf5ac978639ec003c6cb7
Sudhanshu-0907 <sudhanshusingh0907@gmail.com>
2024-02-28 23:44:57 +0530

    chatRoom added and navigation to it added

--- Components/ListChatItem/index.js
+++ Components/ListChatItem/index.js
@@ -2,14 +2,24 @@ import React from "react";
 import {Image, Text, TouchableWithoutFeedback, View} from "react-native";
 import styles from "./style";
 import moment from "moment";
+import {useNavigation} from "@react-navigation/native";
 
 const ListChatItem = (props) => {
     const {chatRoom} = props;
     const user = chatRoom.users[1];
 
+    const navigation = useNavigation();
+
+    const onClick = () => {
+        navigation.navigate('ChatRoom', {
+            id: chatRoom.id,
+            name: user.name
+        })
+    }
+
     return (
         <TouchableWithoutFeedback
-            // onPress={onClick}
+            onPress={onClick}
         >
             <View style={styles.container}>
                 <View style={styles.lefContainer}>
--- navigation/StackNavigator.jsx
+++ navigation/StackNavigator.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import Colors from "../Constant/Color";
 import {View} from "react-native";
-import {createStackNavigator} from '@react-navigation/stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import MainTabNavigator from "./MainTabNavigation";
 import Octicons from "react-native-vector-icons/Octicons";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import ChatRoomScreen from "../Screen/ChatRoomScreen";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function StackNavigator() {
 
     return (
         <Stack.Navigator screenOptions={{
+            // headerShadowVisible: 'false',
+            headerTransparent: 'true',
             headerStyle: {
                 backgroundColor: Colors.light.tint,
                 shadowOpacity: 0,
@@ -42,25 +47,26 @@ export default function StackNavigator() {
                     )
                 }}
             />
-            {/*<Stack.Screen*/}
-            {/*    name="ChatRoom"*/}
-            {/*    component={ChatRoomScreen}*/}
-            {/*    options={({route}) => ({*/}
-            {/*        title: route.params.name,*/}
-            {/*        headerRight: () => (*/}
-            {/*            <View style={{*/}
-            {/*                flexDirection: 'row',*/}
-            {/*                width: 100,*/}
-            {/*                justifyContent: 'space-between',*/}
-            {/*                marginRight: 10,*/}
-            {/*            }}>*/}
-            {/*                <FontAwesome5 name="video" size={22} color={'white'}/>*/}
-            {/*                <MaterialIcons name="call" size={22} color={'white'}/>*/}
-            {/*                <MaterialCommunityIcons name="dots-vertical" size={22} color={'white'}/>*/}
-            {/*            </View>*/}
-            {/*        )*/}
-            {/*    })}*/}
-            {/*/>*/}
+            <Stack.Screen
+                name="ChatRoom"
+                component={ChatRoomScreen}
+                options={({route}) => ({
+                    title: route.params.name,
+                    headerRight: () => (
+                        <View style={{
+                            flexDirection: 'row',
+                            width: 100,
+                            justifyContent: 'space-between',
+                            marginRight: 10,
+                        }}>
+                            <FontAwesome5 name="video" size={22} color={'white'}/>
+                            <MaterialIcons name="call" size={22} color={'white'}/>
+                            <MaterialCommunityIcons name="dots-vertical" size={22} color={'white'}/>
+                        </View>
+                    ),
+                    animation: 'slide_from_right'
+                })}
+            />
             {/*<Stack.Screen*/}
             {/*    name="Contacts"*/}
             {/*    component={ContactsScreen}*/}
